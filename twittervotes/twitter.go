112f92bdfbd4f499115d50b7297164148b313f25
Leonardo Souza <leogsouza@gmail.com>
2017-11-02 01:55:52 +0000

    Implement read from twitter function

--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/joeshaw/envdecode"
 	"github.com/matryer/go-oauth/oauth"
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -89,4 +92,56 @@ func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
 	req.Header.Set("Authorization",
 		authClient.AuthorizationHeader(creds, "POST", req.URL, params))
 	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("Failed to load options:", err)
+		return
+	}
+
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("Creating filter request failed:", err)
+		return
+	}
+
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("Creating filter request failed:", err)
+		return
+	}
+
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("make request failed:", err)
+		return
+	}
+
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(tweet.Text),
+				strings.ToLower(option),
+			) {
+				log.Println("vote:", option)
+				vites <- option
+			}
+		}
+	}
+
 }
